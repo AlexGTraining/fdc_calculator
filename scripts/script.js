366777589c67819845b8f1876d0ca33242587375
Artrediss <canurul@sabanciuniv.edu>
2025-02-05 16:49:28 +0300

    division by zero, rounding long decimals, managing operation state done

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let firstNumber = '';
 let operator = '';
-let newNumber = '';
+let newNumber = true;
 
 const add = (a, b) => {
     return a+b;
@@ -21,19 +21,57 @@ const divide = (a, b) => {
     return a/b;
 }
 
+const operate = (op, a, b) => {
+    a = Number(a);
+    b = Number(b);
+    switch(op) {
+        case '+' : return add(a, b);
+        case '-' : return substract(a, b);
+        case '*' : return multiply(a, b);
+        case '/' : return divide(a, b);
+    }
+}
+
 // This function clears all the values
 function clearScreen() {
-    document.getElementById("result").value = "";
+    document.getElementById("result").value = '';
+    firstNumber = '';
+    operator = '';
+    newNumber = true;
 }
  
 // This function displays the values
 function display(value) {
-    document.getElementById("result").value += value;
+    const display = document.getElementById("result");
+   
+   if (['+', '-', '*', '/'].includes(value)) {
+       operator = value;
+       firstNumber = display.value;
+       newNumber = true;
+       return;
+   }
+   
+   if (newNumber) {
+       display.value = value;
+       newNumber = false;
+   } else {
+       display.value += value;
+   }
 }
  
 // This function evaluates the expression and returns the result
 function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-}
+    if (!operator || !firstNumber) return;
+    
+    const secondNumber = document.getElementById("result").value;
+    const result = operate(operator, firstNumber, secondNumber);
+    
+    if (typeof result === 'number') {
+        document.getElementById("result").value = Math.round(result * 1000) / 1000;
+    } else {
+        document.getElementById("result").value = result;
+    }
+    
+    firstNumber = '';
+    operator = '';
+ }
